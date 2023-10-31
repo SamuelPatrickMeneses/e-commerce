ee485f5cc504f3104383df138dd8f7211580df1b
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-10-31 11:47:22 -0300

    type modify file file: src/views/db.php description: configure to use enviroment variables in database conection.

--- src/views/db.php
+++ src/views/db.php
@@ -1,12 +1,13 @@
 <?php
 
-$host = '';
-$name = '';
-$user = '';
-$password = '';
+$host = $_ENV['DB_HOST'];
+$name = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$port = $_ENV['DB_PORT'];
 
 try {
-	$pdo = new PDO("mysql:host=$host;dbname=$name", $user, $password);
+	$pdo = new PDO("mysql:host=$host;port=$port;dbname=$name", $user, $password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch( PDOException $exception ) {
 	echo "Connection error :" . $exception->getMessage();
